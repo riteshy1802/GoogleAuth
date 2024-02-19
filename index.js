e16d4f93b170e2a5ff5a8aa4bcb7bd10ec31f56a
riteshy1802 <143412534+riteshy1802@users.noreply.github.com>
2024-02-19 19:19:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth,GoogleAuthProvider,signInWithPopup,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBQTk3JZyBUFh6SmJ62PCvbUOA7d2eut30",
+    authDomain: "authentication-50382.firebaseapp.com",
+    projectId: "authentication-50382",
+    storageBucket: "authentication-50382.appspot.com",
+    messagingSenderId: "1005714116978",
+    appId: "1:1005714116978:web:77ad026ff35a40b9c50deb"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  auth.languageCode = "en";
+  const provider = new GoogleAuthProvider();
+
+
+  const googleLogin = document.getElementById("google");
+  googleLogin.addEventListener("click",function(){
+
+   signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user);
+    window.location.href = "./loggedIn.html"
+
+    }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  })
+
+  function updateUserProfile(user){
+    const userName = user.displayName;
+    const userEmail = user.email;
+    const userProfilePicture = user.photoURL;
+
+    document.getElementById("userName").textContent = userName;
+    document.getElementById("userEmail").textContent = userEmail;
+    document.getElementById("userProfilePicture").src = userProfilePicture;
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      updateUserProfile(user);
+    } else {
+      // User is signed out
+      console.log("User is signed out");
+    }
+  });
